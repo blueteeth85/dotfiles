bc3360615b655c05d1db8c6fc5d2e96e50a42af6
Swaminathan Sankararaman <swami2k7@gmail.com>
2014-05-15 17:33:17 -0400

    added linum-mode customizations for emacs

--- emacs/emacs.d.symlink/my-config/general-rc.el
+++ emacs/emacs.d.symlink/my-config/general-rc.el
@@ -12,7 +12,27 @@
 (define-key global-map [(meta down)] 'forward-paragraph)
 (define-key global-map [(meta up)] 'backward-paragraph)
 
+(unless window-system
+  (add-hook 'linum-before-numbering-hook
+	        (lambda ()
+		        (setq-local linum-format-fmt
+				      (let ((w (length (number-to-string
+							    (count-lines (point-min) (point-max))))))
+					    (concat "%" (number-to-string w) "d"))))))
+
+(defun linum-format-func (line)
+  (concat
+   (propertize (format linum-format-fmt line) 'face 'linum)
+   (propertize " " 'face 'mode-line)
+   )
+  )
+
+(unless window-system
+  (setq linum-format 'linum-format-func))
+
+(global-linum-mode 1)
 
 ;; Color theme ;;
 (add-to-list 'custom-theme-load-path "~/.emacs.d/my-packages/emacs-color-theme-solarized")
 ;(load-theme 'solarized-dark t)
+
